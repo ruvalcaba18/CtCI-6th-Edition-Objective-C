ec6bb4a4826f74a06f320db7e5f3afc7e8ca4bac
Jael <Jael.ruvalcaba>
2025-07-28 23:10:41 -0700

    Add helpers to main .m file

--- Ch 02. Linked Lists/Question-3/Q-3/Q-3/main.m
+++ Ch 02. Linked Lists/Question-3/Q-3/Q-3/main.m
@@ -6,27 +6,50 @@
 #import "LinkedListNode.h"
 #import "DeleteMiddleNode.h"
 
+void printList(LinkedListNode *node) {
+    NSMutableString *output = [NSMutableString string];
+    while (node != nil) {
+        [output appendFormat:@"%ld -> ", (long)node.value];
+        node = node.nextNode;
+    }
+    [output appendString:@"nil"];
+    NSLog(@"%@", output);
+}
+
+LinkedListNode* buildTestList(void) {
+    LinkedListNode *node1 = [[LinkedListNode alloc] initWithValue: 1 withNextNode: nil];
+    LinkedListNode *node2 = [[LinkedListNode alloc] initWithValue: 2 withNextNode: node1];
+    LinkedListNode *node3 = [[LinkedListNode alloc] initWithValue: 10 withNextNode: node2];
+    LinkedListNode *node4 = [[LinkedListNode alloc] initWithValue: 5 withNextNode: node3];
+    LinkedListNode *node5 = [[LinkedListNode alloc] initWithValue: 8 withNextNode: node4];
+    LinkedListNode *node6 = [[LinkedListNode alloc] initWithValue :5 withNextNode: node5];
+    LinkedListNode *head = [[LinkedListNode alloc] initWithValue: 3 withNextNode: node6];
+    
+    return head;
+}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // Test Case 1: 3->5->8->5->10->2->1
-        LinkedListNode *node1 = [[LinkedListNode alloc] initWithValue: 1 withNextNode: nil];
-        LinkedListNode *node2 = [[LinkedListNode alloc] initWithValue: 2 withNextNode: node1];
-        LinkedListNode *node3 = [[LinkedListNode alloc] initWithValue: 10 withNextNode: node2];
-        LinkedListNode *node4 = [[LinkedListNode alloc] initWithValue: 5 withNextNode: node3];
-        LinkedListNode *node5 = [[LinkedListNode alloc] initWithValue: 8 withNextNode: node4];
-        LinkedListNode *node6 = [[LinkedListNode alloc] initWithValue :5 withNextNode: node5];
-        LinkedListNode *head = [[LinkedListNode alloc] initWithValue: 3 withNextNode: node6];
-        
+       
+        LinkedListNode *head = buildTestList();
         NSLog(@"Original List:");
-        [head printList];
+        printList(head);
         
         DeleteMiddleNode *deleter = [DeleteMiddleNode new];
-        LinkedListNode *result = [deleter deleteMiddleNodeFrom:head];
+        LinkedListNode *result = [deleter deleteMiddleNodeFrom: head];
         
         NSLog(@"After deleting middle node:");
-        [result printList];
-   
+        printList(result);
+        
+        NSLog(@"Remake list:");
+        head = buildTestList();
+        printList(head);
+        
+        LinkedListNode *newResult = [deleter deleteMiddleNodeKnowingLenghtFrom: head];
+        
+        NSLog(@"After deleting middle node with knowing the lenght:");
+        printList(newResult);
     }
     return 0;
 }
